05fb81d78332386adfa6a48379324040ced447c7
ikhwanox <ikhwantukiran03@gmail.com>
2025-05-24 01:33:28 +0800

    login controller

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,3 @@
-// routes/web.php
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -7,41 +6,52 @@ use App\Http\Controllers\AboutController;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\CertificateController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\LoginController;
 
 // Public routes
-Route::get('/', [Home::class, 'index'])->name('home');
-Route::get('/about', [About::class, 'index'])->name('about.index');
-Route::get('/portfolio', [Portfolio::class, 'index'])->name('portfolio.index');
-Route::get('/portfolio/{id}', [Portfolio::class, 'show'])->name('portfolio.show');
-Route::get('/certificates', [Certificate::class, 'index'])->name('certificate.index');
-Route::get('/certificates/{id}', [Certificate::class, 'show'])->name('certificate.show');
-Route::get('/contact', [Contact::class, 'index'])->name('contact.index');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio.index');
+Route::get('/portfolio/{id}', [PortfolioController::class, 'show'])->name('portfolio.show');
+Route::get('/certificates', [CertificateController::class, 'index'])->name('certificate.index');
+Route::get('/certificates/{id}', [CertificateController::class, 'show'])->name('certificate.show');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
 
-// Admin routes (These should be protected by authentication middleware in a production environment)
-Route::prefix('admin')->group(function () {
+// Auth routes
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Admin routes (protected by auth middleware)
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function() {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    
     // About routes
-    Route::get('/about/create', [About::class, 'create'])->name('about.create');
-    Route::post('/about', [About::class, 'store'])->name('about.store');
-    Route::get('/about/{id}/edit', [About::class, 'edit'])->name('about.edit');
-    Route::put('/about/{id}', [About::class, 'update'])->name('about.update');
+    Route::get('/about/create', [AboutController::class, 'create'])->name('about.create');
+    Route::post('/about', [AboutController::class, 'store'])->name('about.store');
+    Route::get('/about/{id}/edit', [AboutController::class, 'edit'])->name('about.edit');
+    Route::put('/about/{id}', [AboutController::class, 'update'])->name('about.update');
     
     // Portfolio routes
-    Route::get('/portfolio/create', [Portfolio::class, 'create'])->name('portfolio.create');
-    Route::post('/portfolio', [Portfolio::class, 'store'])->name('portfolio.store');
-    Route::get('/portfolio/{id}/edit', [Portfolio::class, 'edit'])->name('portfolio.edit');
-    Route::put('/portfolio/{id}', [Portfolio::class, 'update'])->name('portfolio.update');
-    Route::delete('/portfolio/{id}', [Portfolio::class, 'destroy'])->name('portfolio.destroy');
+    Route::get('/portfolio/create', [PortfolioController::class, 'create'])->name('portfolio.create');
+    Route::post('/portfolio', [PortfolioController::class, 'store'])->name('portfolio.store');
+    Route::get('/portfolio/{id}/edit', [PortfolioController::class, 'edit'])->name('portfolio.edit');
+    Route::put('/portfolio/{id}', [PortfolioController::class, 'update'])->name('portfolio.update');
+    Route::delete('/portfolio/{id}', [PortfolioController::class, 'destroy'])->name('portfolio.destroy');
     
     // Certificate routes
-    Route::get('/certificates/create', [Certificate::class, 'create'])->name('certificate.create');
-    Route::post('/certificates', [Certificate::class, 'store'])->name('certificate.store');
-    Route::get('/certificates/{id}/edit', [Certificate::class, 'edit'])->name('certificate.edit');
-    Route::put('/certificates/{id}', [Certificate::class, 'update'])->name('certificate.update');
-    Route::delete('/certificates/{id}', [Certificate::class, 'destroy'])->name('certificate.destroy');
+    Route::get('/certificates/create', [CertificateController::class, 'create'])->name('certificate.create');
+    Route::post('/certificates', [CertificateController::class, 'store'])->name('certificate.store');
+    Route::get('/certificates/{id}/edit', [CertificateController::class, 'edit'])->name('certificate.edit');
+    Route::put('/certificates/{id}', [CertificateController::class, 'update'])->name('certificate.update');
+    Route::delete('/certificates/{id}', [CertificateController::class, 'destroy'])->name('certificate.destroy');
     
     // Contact routes
-    Route::get('/contact/create', [Contact::class, 'create'])->name('contact.create');
-    Route::post('/contact', [Contact::class, 'store'])->name('contact.store');
-    Route::get('/contact/{id}/edit', [Contact::class, 'edit'])->name('contact.edit');
-    Route::put('/contact/{id}', [Contact::class, 'update'])->name('contact.update');
+    Route::get('/contact/create', [ContactController::class, 'create'])->name('contact.create');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+    Route::get('/contact/{id}/edit', [ContactController::class, 'edit'])->name('contact.edit');
+    Route::put('/contact/{id}', [ContactController::class, 'update'])->name('contact.update');
+    Route::delete('/contact/{id}', [ContactController::class, 'destroy'])->name('contact.destroy');
 });
